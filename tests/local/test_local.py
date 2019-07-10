5e4efe9a63db07662fe5fe530255a3c6f9a135d6
Rohan McGovern <rmcgover@redhat.com>
2019-07-11 09:13:21 +1000

    Fix test failure for artifacts 'latest' symlink

--- tests/local/test_local.py
+++ tests/local/test_local.py
@@ -28,18 +28,16 @@ def test_local_saves_to_artifacts(caplog, tmpdir, monkeypatch):
     collector.append_file("appended-file.txt", u"chunk 1").result()
     collector.append_file("appended-file.txt", b"\nchunk 2\n").result()
 
-    # It should have created an "artifacts" directory
-    artifactsdir = tmpdir.join("artifacts")
-    assert artifactsdir.check()
+    # It should have created an "artifacts/latest" directory/symlink
+    artifactsdir = tmpdir.join("artifacts", "latest")
+    assert artifactsdir.check(dir=True, link=True)
 
-    # It should have created one subdirectory within artifacts
-    subdirs = artifactsdir.listdir()
-    assert len(subdirs) == 1
-    subdir = subdirs[0]
+    # Resolve symlink for later comparison with log messages
+    artifactsdir = artifactsdir.realpath()
 
     # It should have saved push item data as JSONL
     assert (
-        subdir.join("pushitems.jsonl").open().read()
+        artifactsdir.join("pushitems.jsonl").open().read()
         == textwrap.dedent(
             """
             {"filename": "file1", "state": "PUSHED"}
@@ -50,18 +48,18 @@ def test_local_saves_to_artifacts(caplog, tmpdir, monkeypatch):
     )
 
     # It should have saved the text file with requested content
-    assert subdir.join("some-file.txt").open().read() == "Hello again\n"
+    assert artifactsdir.join("some-file.txt").open().read() == "Hello again\n"
 
     # It should have saved the binary file with requested content
-    assert subdir.join("some-file.bin").open("rb").read() == b"\x00\x01\x02"
+    assert artifactsdir.join("some-file.bin").open("rb").read() == b"\x00\x01\x02"
 
     # It should have saved the appended-text file with requested content
-    assert subdir.join("appended-file.txt").open().read() == "chunk 1\nchunk 2\n"
+    assert artifactsdir.join("appended-file.txt").open().read() == "chunk 1\nchunk 2\n"
 
     # It should have logged about the created files
     assert caplog.messages == [
-        "Logging to %s" % subdir.join("pushitems.jsonl"),
-        "Logging to %s" % subdir.join("some-file.txt"),
-        "Logging to %s" % subdir.join("some-file.bin"),
-        "Logging to %s" % subdir.join("appended-file.txt"),
+        "Logging to %s" % artifactsdir.join("pushitems.jsonl"),
+        "Logging to %s" % artifactsdir.join("some-file.txt"),
+        "Logging to %s" % artifactsdir.join("some-file.bin"),
+        "Logging to %s" % artifactsdir.join("appended-file.txt"),
     ]
