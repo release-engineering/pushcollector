3b9e56d60baa32775a78c623363375d33682b21c
Rohan McGovern <rmcgover@redhat.com>
2019-07-23 11:24:48 +1000

    Prepare for version 1.0.0
    No issues with API raised during review period, so let's commit
    to maintaining the API under semver rules from now.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements():
 
 setup(
     name="pushcollector",
-    version="0.1.0",
+    version="1.0.0",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
@@ -31,7 +31,7 @@ setup(
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Programming Language :: Python :: 2",
